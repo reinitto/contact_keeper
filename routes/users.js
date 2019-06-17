faeaf60f65b0be00ce952c355990e37ba349fff4
Labais <reinitto@gmail.com>
2019-06-17 18:19:47 +0200

    remove config

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const config = require('config');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
-const connectDB = require('./config/db');
+const mongoose = require('mongoose');
+
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 //Connect Database
+
+const db = process.env.MONGO_URI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
 connectDB();
 
 //init Middleware
@@ -17,12 +35,12 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
 //Serve static assets in production
-// if (process.env.NODE_ENV === 'production') {
-//Set static folder
-app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
 
-app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-);
-
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
